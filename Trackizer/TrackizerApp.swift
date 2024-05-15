3a86dc41b74960a31c6bdce301dc1348ab2089a0
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-05-15 12:24:12 -0600

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by Modamori Oluwayomi on 2024-05-15.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
